fb851e705798278eeaeac1b8d2de6301786cf9af
Kiet <ntuankiet695@gmail.com>
2016-10-03 21:46:34 +0700

    Rendering Partial Collections

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,32 @@
+class CommentsController < ApplicationController
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+ 
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
+  end
+ 
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
+end
+
+
+class CreateComments < ActiveRecord::Migration[5.0]
+	def change
+    create_table :comments do |t|
+      t.string :commenter
+      t.text :body
+      t.references :article, foreign_key: true
+ 
+      t.timestamps
+    end
+  end
+end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,4 @@
 class Comment < ApplicationRecord
   belongs_to :article
-  validates :title, presence: true,
-                    length: { minimum: 5 }
+ 
 end
